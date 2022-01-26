ea345e55b271c203a11a2a1a9c2d96894980d81d
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-01-25 22:16:48 -0500

    Manage user input with states
    1. Manage user input with states
    2. Add some validation on input

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import addUserClasses from "./AddUser.module.css";
 
@@ -7,22 +7,52 @@ import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsernam] = useState("");
+  const [enteredLevel, setEnteredLevel] = useState(0);
+
+  const usernameInputChangeHandler = (event) => {
+    setEnteredUsernam(event.target.value);
+  };
+
+  const levelInputChangeHandler = (event) => {
+    setEnteredLevel(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
-    /*         if (enteredValue.trim().length === 0) {
-          setIsInputValid(false);
-          return;
-        }
-        props.onAddGoal(enteredValue) */
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredLevel.trim().length === 0
+    ) {
+      console.log("Incomplete Input");
+      return;
+    }
+    if (+enteredLevel < 1) {
+      console.log("Invalid Level");
+      return;
+    }
+    console.log(enteredUsername, enteredLevel);
+    setEnteredUsernam("");
+    setEnteredLevel(0);
   };
 
   return (
     <Card className={addUserClasses.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameInputChangeHandler}
+        />
         <label htmlFor="level">Level</label>
-        <input id="level" type="number" />
+        <input
+          id="level"
+          type="number"
+          value={enteredLevel}
+          onChange={levelInputChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
