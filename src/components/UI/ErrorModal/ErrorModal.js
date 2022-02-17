f79d277ca179cd2ca8c7486844965e02a6b2d2f6
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-02-16 23:23:47 -0500

    rewrite error modal with portal
    rewrite error modal with portal and put it directly under DOM body

--- src/components/UI/ErrorModal/ErrorModal.js
+++ src/components/UI/ErrorModal/ErrorModal.js
@@ -3,22 +3,43 @@ import React from "react";
 import Classes from "./ErrorModal.module.css";
 import Card from "../Card/Card";
 import Button from "../Button/Button";
+import ReactDOM from "react-dom";
+
+const BackDrop = (props) => {
+  return <div className={Classes.backdrop} onClick={props.onReturn2Input} />;
+};
+
+const OverLay = (props) => {
+  return (
+    <Card className={Classes.modal}>
+      <header className={Classes.header}>
+        <h2>{props.errortitle}</h2>
+      </header>
+      <div className={Classes.content}>
+        <p>{props.errorMessage}</p>
+      </div>
+      <footer className={Classes.actions}>
+        <Button onClick={props.onReturn2Input}>Close</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
     <>
-      <div className={Classes.backdrop} onClick={props.onReturn2Input} />
-      <Card className={Classes.modal}>
-        <header className={Classes.header}>
-          <h2>{props.errortitle}</h2>
-        </header>
-        <div className={Classes.content}>
-          <p>{props.errorMessage}</p>
-        </div>
-        <footer className={Classes.actions}>
-          <Button onClick={props.onReturn2Input}>Close</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <BackDrop onReturn2Input={props.onReturn2Input} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <OverLay
+          errortitle={props.errortitle}
+          errorMessage={props.errorMessage}
+          onReturn2Input={props.onReturn2Input}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
